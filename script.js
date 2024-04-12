c5a3475525220be0e259a7da3d8098d1e7d20aab
Bharat <bharat18.tech@gmail.com>
2024-04-12 11:13:19 +0530

    rename files & update script.js

--- script.js
+++ script.js
@@ -7,6 +7,15 @@ function increment() {
     countEl.textContent = count
 }
 
+function decrement() {
+    if(count>0){
+        count = count - 1;
+    } else {
+        count = 0;
+    }
+    countEl.textContent = count
+}
+
 function save() {
     let countStr = count + " - "
     saveEl.textContent += countStr
